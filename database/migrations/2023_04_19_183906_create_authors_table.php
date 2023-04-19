e46252b03300a03f0984f793147c2fa0a361785d
Keisha55 <l.kimeto@rochman-properties.co.ke>
2023-04-19 21:46:27 +0300

    created authors table

---
+++ database/migrations/2023_04_19_183906_create_authors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('github');
+            $table->string('twitter');
+            $table->string('location');
+            $table->string('latest_article_published');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('authors');
+    }
+};
